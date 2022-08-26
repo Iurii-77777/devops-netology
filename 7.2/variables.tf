496fa838dc813880e6d720aff302a3b0e6212223
Iurii-77777 <90862571+Iurii-77777@users.noreply.github.com>
2022-08-26 18:13:28 +0300

    Add files via upload

---
+++ 7.2/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gp9k6jt4bbkkfr2vup"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gi8uu2eq7nih1vp6dq"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd80q5979r924k9hjo7t"
+}
